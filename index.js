bd6949af176eabb8235a21849c9529213483c31c
Pearce Odegard <69996490+podegard89@users.noreply.github.com>
2022-12-08 13:55:33 -0600

    Created window load event, imported JS, minor HTML edits

---
+++ index.js
@@ -0,0 +1,12 @@
+// The window object represents the browser 
+// tab that we are loading in
+// We add this event listener so that we can make sure the window
+// loaded before we
+// start interacting with the HTML content on the 
+// page with our JS code
+window.addEventListener("load", function () {
+    // Any code inside of this function will execute after
+    // the window loads.
+
+
+});
